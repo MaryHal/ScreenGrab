0c48a8562f5bb36d77136f43772ab9a3c705072a
MaryHal <maryhal@users.noreply.github.com>
2013-08-14 08:40:17 -0700

    Dependency checks

--- ScreenGrab.rb
+++ ScreenGrab.rb
@@ -30,9 +30,34 @@ def capture(options)
     `ffmpeg -f x11grab -r #{options[:fps]} -i :0.0 -f alsa -i hw:0,0 -acodec flac -vcodec ffvhuff #{options[:output]}`
 end
 
+def which(cmd)
+  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+    exts.each { |ext|
+      exe = File.join(path, "#{cmd}#{ext}")
+      return exe if File.executable? exe
+    }
+  end
+  return nil
+end
+
+def checkDependencies()
+    if which("xdpyinfo").nil?
+        raise "xdpyinfo is required to run this script."
+    end
+
+    if !which("ffmpeg").nil?
+        return "ffmpeg"
+    elsif !which("avconv").nil?
+        return "avconv"
+    end
+    raise "ffmpeg or avconv required to run this script."
+end
+
 # Main script
 if __FILE__ == $0
-    puts options
-    capture(options)
+    puts checkDependencies
+    #puts options
+    #capture(options)
 end
 
