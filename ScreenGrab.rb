601be292342e1955718ef89a27aa851f02f1c50e
MaryHal <maryhal@users.noreply.github.com>
2013-08-13 22:17:14 -0700

    Frame rate option

--- ScreenGrab.rb
+++ ScreenGrab.rb
@@ -6,15 +6,15 @@ options = {}
 
 # Parse command line options
 optparse = OptionParser.new do|opts|
-    #opts.banner = "Usage: passgen [options] [length of passwords to generate]"
+    #opts.banner = ""
 
     options[:output] = "./test.mkv"
     opts.on( '-o', '--output file', 'Output File' ) do|output|
         options[:output] = output
     end
 
-    options[:fps] = "./test.mkv"
-    opts.on( '-r', '--rate fps', 'Frames Per Second' ) do|fps|
+    options[:fps] = 30
+    opts.on( '-r', '--rate fps', Integer, 'Frames Per Second' ) do|fps|
         options[:fps] = fps
     end
 
@@ -24,11 +24,11 @@ optparse = OptionParser.new do|opts|
     end
 end
 
+optparse.parse!
+
 def capture(options)
     `ffmpeg -f x11grab -r #{options[:fps]} -i :0.0 -f alsa -i hw:0,0 -acodec flac -vcodec ffvhuff #{options[:output]}`
 end
-
-optparse.parse!
 
 # Main script
 if __FILE__ == $0
