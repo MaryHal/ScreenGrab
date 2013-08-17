24fad8a0481da5023a8b7e456d8444d6480f917d
MaryHal <maryhal@users.noreply.github.com>
2013-08-16 20:36:36 -0700

    More settings, pretty simple, but working, version

--- ScreenGrab.rb
+++ ScreenGrab.rb
@@ -2,6 +2,26 @@
 
 require 'optparse'
 
+vdevices = Hash.new
+vdevices["none"] = []
+vdevices["desktop"] = ["-f", "x11grab", "-i", ":0.0"]
+vdevices["webcam"]  = ["-f", "v4l2", "-i", "/dev/video0"]
+
+vcodecs = Hash.new
+vcodecs["raw"]        = ["-vcodec", "ffvhuff"]
+vcodecs["h264_ultra"] = ["-vcodec", "libx264", "-preset", "ultrafast", "-qp", "0"]
+vcodecs["h264_slow"]  = ["-vcodec", "libx264", "-preset", "veryslow", "-qp", "0"]
+vcodecs["theora"]     = ["-vcodec", "libtheora", "-b:v", "4000k"]
+
+adevices = Hash.new
+adevices["none"] = []
+adevices["alsa"] = ["-f", "alsa", "-i", "hw:0,0"]
+
+acodecs = Hash.new
+acodecs["flac"]   = ["-acodec", "flac"]
+acodecs["vorbis"] = ["-acodec", "libvorbis"]
+acodecs["mp3"]    = ["-acodec", "libmp3lame"]
+
 # Parse command line options
 options = {}
 optparse = OptionParser.new do|opts|
@@ -12,10 +32,35 @@ optparse = OptionParser.new do|opts|
         options[:output] = output
     end
 
+    options[:acodec] = "vorbis"
+    acodecList = acodecs.keys.join(', ')
+    opts.on( '-a', '--acodec codec', acodecList, 'Audio Codec' ) do|codec|
+        options[:acodec] = codec
+    end
+
+    options[:adevice] = "alsa"
+    adeviceList = adevices.keys.join(', ')
+    opts.on( '-ad', '--adevice device', adeviceList, 'Audio Device' ) do|device|
+        options[:adevice] = device
+    end
+
+    options[:vcodec] = "theora"
+    vcodecList = vcodecs.keys.join(', ')
+    opts.on( '-v', '--vcodec codec', vcodecList, 'Video Codec' ) do|codec|
+        options[:vcodec] = codec
+    end
+
+    options[:vdevice] = "desktop"
+    vdeviceList = vdevices.keys.join(', ')
+    opts.on( '-vd', '--vdevice device', vdeviceList, 'Video Device' ) do|device|
+        options[:vdevice] = device
+    end
+
     options[:fps] = 30
     opts.on( '-r', '--rate fps', Integer, 'Frames Per Second' ) do|fps|
         options[:fps] = fps
     end
+
 
     opts.on( '-h', '--help', 'Display this screen' ) do
         puts opts
@@ -24,33 +69,6 @@ optparse = OptionParser.new do|opts|
 end
 
 optparse.parse!
-
-vcodecs = Hash.new
-vcodecs[:h264_fast] = []
-
-acodecs = Hash.new
-acodecs[:flac] = []
-
-def captureCommand(exe)
-    command = [exe,
-               # Audio Settings
-               "-f",      "alsa",
-               "-ac",     "2",       # Two audio channels
-               "-i",      "hw:0,0",
-               "-acodec", "flac",
-
-               # Video Settings
-               "-f",      "x11grab",
-               "-r",      "30",      # Frame Rate
-               "-i",      ":0.0",
-               "-vcodec",  "libtheora",
-               "-b:v",     "4000k",
-
-               # Output file
-               "test.mkv"
-    ]
-    return command.join(' ')
-end
 
 # Dependency Checking
 def which(cmd)
@@ -65,8 +83,8 @@ def which(cmd)
 end
 
 def checkDependencies()
-    if which("xdpyinfo").nil?
-        raise "xdpyinfo is required to run this script."
+    if which("xwininfo").nil?
+        raise "xwininfo is required to run this script."
     end
 
     if !which("ffmpeg").nil?
@@ -80,9 +98,15 @@ end
 # Main script
 if __FILE__ == $0
     puts options
-
     exe = checkDependencies
-    command = captureCommand(exe)
+
+    command = [exe] + 
+              adevices[options[:adevice]] +
+              acodecs[options[:acodec]] +
+              vdevices[options[:vdevice]] +
+              vcodecs[options[:vcodec]] +
+              [options[:output]]
+    command = command.join(' ')
     `#{command}`
 end
 
